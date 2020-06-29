6fbfe7537de490f34da7326192d50121c68c8a36
onecaym <osipov99@list.ru>
2020-06-29 23:06:47 +0400

    dofferent improvements

--- checker.rb
+++ checker.rb
@@ -2,37 +2,37 @@ require 'csv'
 require 'fileutils'
 require 'digest/sha1'
 
-# This class gives one instrument for finding a few repetitive files inside given folder
-# @example
-# checker = Checker.new('folder/')
-# Every folder shouldn't have a subfolder
 class Checker
   def initialize(folder)
     @folder = folder
   end
 
-  private
+  def content_of_given_folder
+    Dir.children(@folder)
+  end
 
-  # @private
-  def file_code
-    folder_content = Dir.children(@folder)
-      hash_code = folder_content.map do |file_name|
-        { file_name => (Digest::SHA1.hexdigest File.read("#{@folder}#{file_name}")) }
+  def file_reader(dir,file_name)
+    File.read("#{dir}#{file_name}")
+  end
+
+  def sha1_encoder(dir,file_name)
+    Digest::SHA1.hexdigest file_reader(dir,file_name)
+  end
+
+  def hash_converter
+      content_of_given_folder.map do |file_name|
+        { file_name => sha1_encoder(@folder,file_name)}
       end
   end
 
-  # @private
   def repetitive_value
-    uniq_value = file_code.map { |hash| hash.values.join }
+    uniq_value = hash_converter.map { |hash| hash.values.join }
     uniq_value.detect { |code| uniq_value.count(code) > 1 }
   end
 
-  public
 
-  # Returns a full list of repetitive files
-  # @return [String]
   def check
-      file_code.map do |hash|
+      hash_converter.map do |hash|
         if repetitive_value == hash.values.first
           puts "#{hash.keys.first} - Использован несколько раз"
         else
